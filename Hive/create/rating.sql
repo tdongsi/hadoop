d9852234f94aaa2b0c0b7aebb51f66eeee33b194
tdongsi <dongsi.tuecuong@gmail.com>
2015-01-21 17:13:28 -0800

    Add data files for movie rating database and load first table

--- Hive/create/rating.sql
+++ Hive/create/rating.sql
@@ -1,11 +1,26 @@
-/* Based on DDL scripts in https://github.com/tdongsi/databases/tree/master/sql/create */
-/* Drop the database if it exists */
+-- Based on DDL scripts in https://github.com/tdongsi/databases/tree/master/sql/create
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS rating CASCADE;
 
-/* Drop the tables if they already exists */
+-- Drop the tables if they already exists: taken care by CASCADE
 
-/* Create the database */
+-- Create the database
+CREATE DATABASE rating;
+USE rating;
 
-/* Create the schema for the tables */
+-- Create the schema for the tables
+CREATE TABLE Movie (
+mID INT comment 'Movie ID',
+title STRING comment 'Movie title',
+year INT comment 'Release year',
+director STRING comment 'Director name'
+)
+COMMENT 'List of movies'
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ', '
+STORED AS TEXTFILE;
 
-/* Populate the tables with data */
+-- Populate the tables with data
+LOAD DATA LOCAL INPATH './movie.data' OVERWRITE INTO TABLE Movie;
 
+-- Show table
+SELECT * FROM Movie;
