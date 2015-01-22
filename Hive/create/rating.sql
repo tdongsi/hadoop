1e0adead5ce838fb0043818fe8984721d6e65390
tdongsi <dongsi.tuecuong@gmail.com>
2015-01-21 18:11:52 -0800

    Add tables for Reviewer and Rating. Add SerDe to remove quote around string in csv data

--- Hive/create/rating.sql
+++ Hive/create/rating.sql
@@ -19,8 +19,48 @@ COMMENT 'List of movies'
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE;
 
+CREATE TABLE Reviewer (
+rID INT comment 'Reviewer ID',
+name STRING comment 'Reviewer name'
+)
+COMMENT 'List of reviewers'
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+STORED AS TEXTFILE;
+
+
+CREATE TABLE Rating_Stage (
+rID INT comment 'Movie ID',
+mID INT comment 'Reviewer ID',
+stars INT comment 'Rating',
+ratingDate STRING comment 'Date of review'
+)
+COMMENT 'Staging table for ratings'
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+   "quoteChar"     = "'"
+)
+STORED AS TEXTFILE;
+
+CREATE TABLE Rating (
+rID INT comment 'Movie ID',
+mID INT comment 'Reviewer ID',
+stars INT comment 'Rating',
+ratingDate BIGINT comment 'Date of review'
+)
+COMMENT 'List of ratings';
+
 -- Populate the tables with data
 LOAD DATA LOCAL INPATH './movie.data' OVERWRITE INTO TABLE Movie;
+LOAD DATA LOCAL INPATH './reviewer.data' OVERWRITE INTO TABLE Reviewer;
+LOAD DATA LOCAL INPATH './rating.data' OVERWRITE INTO TABLE Rating_Stage;
+
+-- TODO: Fix the error
+-- FROM Rating_Stage rs
+-- INSERT OVERWRITE TABLE Rating
+-- SELECT rs.rID, rs.mID, rs.stars, unix_timestamp(rs.ratingDate, 'yyyy-MM-dd');
 
 -- Show tables
 SELECT * FROM Movie;
+SELECT * FROM Reviewer;
+SELECT * FROM Rating_Stage;
+-- SELECT * FROM Rating;
